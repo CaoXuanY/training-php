c76d9e08337b70bf2bdf0338d929d9db2c24818e
xuany <caoxuany16051980@gmail.com>
2021-11-28 07:27:07 +0700

    upfile delete

--- delete_user.php
+++ delete_user.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
@@ -7,7 +8,24 @@ $id = NULL;
 
 if (!empty($_GET['id'])) {
     $id = $_GET['id'];
-    $userModel->deleteUserById($id);//Delete existing user
+    var_dump($id);
+    //Get first number
+    $start = substr($id, 0, 5);
+
+    //Get last number
+    $end = substr($id, -5);
+
+    //Replace first number with null
+    $id = str_replace($start, "", $id);
+
+    //Replace last number with null
+    $id = str_replace($end, "", $id);
+   // var_dump($id); die;
+   //so sánh 2 mã token:fix lỗi csrf
+   if($_GET['token'] == $_SESSION['token']){
+         $userModel->deleteUserById($id);//Delete existing user
+  }
+
 }
 header('location: list_users.php');
 ?>
