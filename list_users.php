74c8f4e676966a839a715fb3470489b23c0501a5
xuany <caoxuany16051980@gmail.com>
2021-11-28 07:29:36 +0700

    Update list_users

--- list_users.php
+++ list_users.php
@@ -9,6 +9,8 @@ $params = [];
 if (!empty($_GET['keyword'])) {
     $params['keyword'] = $_GET['keyword'];
 }
+$token = md5(uniqid());
+// var_dump($token);
 
 $users = $userModel->getUsers($params);
 ?>
@@ -50,13 +52,14 @@ $users = $userModel->getUsers($params);
                                 <?php echo $user['type']?>
                             </td>
                             <td>
-                                <a href="form_user.php?id=<?php echo $user['id'] ?>">
+                                <a href="form_user.php?id=<?php echo rand(10000, 99999) . $user['id'] . rand(10000, 99999) ?>">
                                     <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i>
                                 </a>
-                                <a href="view_user.php?id=<?php echo $user['id'] ?>">
+                                <a href="view_user.php?id=<?php echo rand(10000, 99999) . $user['id'] . rand(10000, 99999) ?>">
                                     <i class="fa fa-eye" aria-hidden="true" title="View"></i>
                                 </a>
-                                <a href="delete_user.php?id=<?php echo $user['id'] ?>">
+                                <a href="delete_user.php?id=<?php echo rand(10000, 99999) . $user['id'] . rand(10000, 99999) ?>&token=<?php echo $token?>">
+                                <?php $_SESSION['token'] = $token; ?>
                                     <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
                                 </a>
                             </td>
