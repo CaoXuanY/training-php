ccaceb07b24b0328fc9037a63f2ccf981376add9
xuany <caoxuany16051980@gmail.com>
2021-12-12 07:39:05 +0700

    UP proxy

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,75 +1,112 @@
 <?php
-require_once 'configs/database.php';
 
-abstract class BaseModel {
-    // Database connection
-    protected static $_connection;
-    protected static $_instance;
 
-    public function __construct() {
+require_once 'BaseModel.php';
 
-        if (!isset(self::$_connection)) {
-            self::$_connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
-            if (self::$_connection->connect_errno) {
-                printf("Connect failed");
-                exit();
-            }
-        }
+class BankModel extends BaseModel {
 
+    public function findBankById($id) {
+        $sql = 'SELECT * FROM banks WHERE id = '.$id;
+        $user = $this->select($sql);
+
+        return $user;
+    }
+
+    public function findUser($keyword) {
+        $sql = 'SELECT * FROM banks WHERE cost LIKE %'.$keyword.'%'. ' OR user_id LIKE %'.$keyword.'%' ;
+        $user = $this->select($sql);
+
+        return $user;
     }
 
     /**
-     * Query in database
-     * @param $sql
+     * Authentication user
+     * @param $userName
+     * @param $password
+     * @return array
      */
-    protected function query($sql) {
-
-        $result = self::$_connection->query($sql);
-        return $result;
+    public function auth($userName, $password) {
+        $md5Password = md5($password);
+        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "'.$md5Password.'"';
+        $user = $this->select($sql);
+        return $user;
     }
 
     /**
-     * Select statement
-     * @param $sql
-     */
-    protected function select($sql) {
-        $result = $this->query($sql);
-        $rows = [];
-        if (!empty($result)) {
-            while ($row = $result->fetch_assoc()) {
-                $rows[] = $row;
-            }
-        }
-        return $rows;
-    }
-
-    /**
-     * Delete statement
-     * @param $sql
+     * Delete user by id
+     * @param $id
      * @return mixed
      */
-    protected function delete($sql) {
-        $result = $this->query($sql);
-        return $result;
+    public function deleteBankById($id) {
+      
+        $sql = 'DELETE FROM banks WHERE id = '.$id;
+        return $this->delete($sql);
+
     }
 
+
+
     /**
-     * Update statement
-     * @param $sql
+     * Update user
+     * @param $input
      * @return mixed
      */
-    protected function update($sql) {
-        $result = $this->query($sql);
-        return $result;
+    public function updateBank($input) {
+        $userModel = new UserModel();
+        if($input['name'] != null){
+            $bankModel->updateUser($input);
+        }else{
+            
+        $sql = 'UPDATE banks SET user_id ="'.$input['user_id'].'", cost = "'.$input['cost'].'" WHERE id = "'.$input['id'].'"';
+
+        $bank = $this->update($sql);
+        return $bank;
+        }
     }
 
     /**
-     * Insert statement
-     * @param $sql
+     * Insert user
+     * @param $input
+     * @return mixed
      */
-    protected function insert($sql) {
-        $result = $this->query($sql);
-        return $result;
+    public function insertBank($input) {
+        
+        $userModel = new UserModel();
+        if($input['name'] != null){
+            $bankModel->insertUser($input);
+        }else{
+        $sql = 'INSERT INTO app_web1.banks (user_id,cost) VALUES ('.$input['user_id'].', '.$input['cost'].')';
+        // var_dump($sql);
+        // die();
+        $bank = $this->insert($sql);
+        return $bank;
+        
+        }   
     }
 
+   /**
+     * Search banks
+     * @param array $param
+     * @return array
+     */
+    public function getBanks($params = []) {
+        //Keyword
+       
+        if (!empty($params['keyword'])) {
+           
+            $sql = 'SELECT * FROM banks WHERE cost LIKE "%' . $params['keyword'] .'%"';
+
+            //Keep this line to use Sql Injection
+            //Don't change
+            //Example keyword: abcef%";TRUNCATE banks;##
+            // $banks = self::$_connection->multi_query($sql);
+            $banks = $this->select($sql);
+            
+        } else {
+            $sql = 'SELECT * FROM banks';
+            $banks = $this->select($sql);
+        }
+        return $banks;
+       
+    }
 }
